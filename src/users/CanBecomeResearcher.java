d4b0caeb7714b368d76be381cf69f5668d52601a
anaraarman <anaraarman@localhost>
2024-12-09 16:42:02 +0500

    added interfaces in users

---
+++ src/users/CanBecomeResearcher.java
@@ -0,0 +1,5 @@
+package users;
+
+public interface CanBecomeResearcher {
+    void becomeResearcher();
+}
---
+++ src/users/CanResearch.java
@@ -0,0 +1,13 @@
+package users;
+
+import java.util.Vector;
+import java.util.Comparator;
+
+public interface CanResearch {
+	
+	Vector<ResearchPaper> printPaper(Comparator<ResearchPaper> comparator);
+    int calculateIndex();
+    void publishPaper(String title, Vector<Researcher> authors, Vector<String> keywords);
+    void proposeProject(String name, Vector<Researcher> team, Vector<String> description);
+    FundingRequest requestFunding(double amount);
+}
