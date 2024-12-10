5e74990851ace8633077ba647d2e2ca3a4c78520
anaraarman <anaraarman@localhost>
2024-12-10 11:08:10 +0500

    added menus

--- src/menu/RoleBasedMenu.java
+++ src/menu/RoleBasedMenu.java
@@ -1,144 +1,71 @@
-package menu
+package menu;
 
-diagram;
+import java.util.Map;
+import enums.Options;
+import enums.UserType;
 
-
-/**
-* @generated
-*/
 public class RoleBasedMenu {
-    
-    /**
-    * @generated
-    */
-    private Map<Options,Runnable>  managerMenu;
-    
-    /**
-    * @generated
-    */
+
+    private Map<Options,Runnable> managerMenu;
     private Map<Options,Runnable> teacherMenu;
-    
-    /**
-    * @generated
-    */
     private Map<Options,Runnable> studentMenu;
-    
-    /**
-    * @generated
-    */
     private Map<Options,Runnable> adminMenu;
-    
-    /**
-    * @generated
-    */
     private Map<Options,Runnable> researcherMenu;
-    
-    
-    
-    /**
-    * @generated
-    */
-    private Map<Options,Runnable> get managerMenu() {
-        return this. managerMenu;
+
+    private Map<Options,Runnable> getManagerMenu() {
+        return this.managerMenu;
     }
-    
-    /**
-    * @generated
-    */
-    private Map<Options,Runnable> set managerMenu(Map<Options,Runnable>  managerMenu) {
-        this. managerMenu =  managerMenu;
+
+    private void setManagerMenu(Map<Options,Runnable> managerMenu) {
+        this.managerMenu = managerMenu;
     }
-    
-    /**
-    * @generated
-    */
+
     private Map<Options,Runnable> getTeacherMenu() {
         return this.teacherMenu;
     }
-    
-    /**
-    * @generated
-    */
-    private Map<Options,Runnable> setTeacherMenu(Map<Options,Runnable> teacherMenu) {
+
+    private void setTeacherMenu(Map<Options,Runnable> teacherMenu) {
         this.teacherMenu = teacherMenu;
     }
-    
-    /**
-    * @generated
-    */
+
     private Map<Options,Runnable> getStudentMenu() {
         return this.studentMenu;
     }
-    
-    /**
-    * @generated
-    */
-    private Map<Options,Runnable> setStudentMenu(Map<Options,Runnable> studentMenu) {
+
+    private void setStudentMenu(Map<Options,Runnable> studentMenu) {
         this.studentMenu = studentMenu;
     }
-    
-    /**
-    * @generated
-    */
+
     private Map<Options,Runnable> getAdminMenu() {
         return this.adminMenu;
     }
-    
-    /**
-    * @generated
-    */
-    private Map<Options,Runnable> setAdminMenu(Map<Options,Runnable> adminMenu) {
+
+    private void setAdminMenu(Map<Options,Runnable> adminMenu) {
         this.adminMenu = adminMenu;
     }
-    
-    /**
-    * @generated
-    */
+
     private Map<Options,Runnable> getResearcherMenu() {
         return this.researcherMenu;
     }
-    
-    /**
-    * @generated
-    */
-    private Map<Options,Runnable> setResearcherMenu(Map<Options,Runnable> researcherMenu) {
+
+    private void setResearcherMenu(Map<Options,Runnable> researcherMenu) {
         this.researcherMenu = researcherMenu;
     }
-    
 
-    //                          Operations                                  
-    
-    /**
-    * @generated
-    */
-    public void displayMenu() {
-        //TODO
+    public void displayMenu(UserType userType) {
+        // TODO: 
+    }
+
+    public void processOption(UserType userType, Integer option) {
+        // TODO: 
+    }
+
+    public Map<Options,Runnable> getMenuRole() {
+        // TODO: 
         return null;
     }
-    
-    /**
-    * @generated
-    */
-    public void processOption() {
-        //TODO
-        return null;
-    }
-    
-    /**
-    * @generated
-    */
-    private Map<Options,Runnable> getMenuRole() {
-        //TODO
-        return null;
-    }
-    
-    /**
-    * @generated
-    */
+
     public void views() {
-        //TODO
-        return null;
+        // TODO: 
     }
-    
-    
 }
